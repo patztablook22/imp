df63ad5c21c8b74e1586498fcf56926a3ef00aa5
patztablook22 <patz@tuta.io>
2020-08-28 16:28:25 +0200

    debug format

--- src/lib/debug.rb
+++ src/lib/debug.rb
@@ -1,6 +1,9 @@
+require_relative 'msg'
+
 module Debug
 
-  @@active
+  @@active = false    # debug mode active
+  @@used   = false    # has it been used?
 
   # print debug message (if set on)
   #
@@ -8,20 +11,19 @@ module Debug
   # Debug> processed(@file)
   #
   def self.> str
-    return unless @@active
-    STDERR.puts "_" * 48
-    STDERR.puts
-    STDERR.puts str
-    exit
-  end
 
-  # debug head
-  # 
-  # Debug.h "processing files"
-  #
-  def self.h str
     return unless @@active
-    STDERR.puts str
+    unless @@used
+      @@used = true
+      STDERR.puts
+      STDERR.puts
+      STDERR.puts "IMP DEBUG"
+      STDERR.puts "─" * Msg.width
+      STDERR.puts
+    end
+
+    pp str
+
   end
 
   # set debug mode
@@ -37,6 +39,10 @@ module Debug
   # set debug hook
   def self.hook str
     return false unless @@active
+    if @@used
+      puts
+      exit 3
+    end
     STDERR.puts str
     true
   end
