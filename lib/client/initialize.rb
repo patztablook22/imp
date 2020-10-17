5ea8ba42c607a9639f7eceabb6cf8ba7b93b511a
patztablook22 <patz@tuta.io>
2020-10-17 23:25:48 +0200

    client

--- lib/client/initialize.rb
+++ lib/client/initialize.rb
@@ -2,7 +2,31 @@ module IMP
   class Client
 
     def initialize
-      @sock = TCPSocket.new("localhost", PORT)
+
+      @sock = TCPSocket.new('localhost', PORT)
+
+      # initial ping
+      @sock.gets
+
+      # now just receive responses
+      Thread.new do
+        listener
+      end
+
+    end
+
+    def listener
+
+      @requests = Hash.new
+
+      loop do
+        response
+      rescue IOError
+        close
+        break
+      rescue
+      end
+
     end
 
   end
